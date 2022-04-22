4b9da0e6213c208f29b7b0dbbd74f82acfcbf050
Jamie Richmond <jamesrichmond@bulb.co.uk>
2022-04-22 17:26:27 +0100

    Modify server and client to use betterproto

--- account_client/account_client/account_client.py
+++ account_client/account_client/account_client.py
@@ -1,56 +1,21 @@
-import logging
-from typing import Iterator
+import asyncio
+import account
 
-import grpc
-import account_pb2
-import account_pb2_grpc
+from grpclib.client import Channel
 
 
-def get_account(stub: account_pb2_grpc.AccountStub, account_id: int):
-    response: account_pb2.GetAccountResponse = stub.GetAccount(
-        account_pb2.GetAccountRequest(account_id=account_id)
-    )
-
-    # This demonstrates how we can check whether an optional field is set
-    has_email_field = response.HasField("email")
-    print(f"has_email_field? {has_email_field}")
+async def main():
+    channel = Channel(host="127.0.0.1", port=50051)
+    service = account.AccountStub(channel)
+    response = await service.get_account(account_id=123)
     print(response)
 
+    async for response in service.stream_industry_ids(account_id=789):
+        print(response)
 
-def stream_industry_ids(stub: account_pb2_grpc.AccountStub, account_id: int):
-    responses: Iterator[account_pb2.StreamIndustryIdsResponse] = stub.StreamIndustryIds(
-        account_pb2.StreamIndustryIdsRequest(account_id=account_id)
-    )
-
-    # We don't have to send everything in one go.
-    # We can stream them and iterate over an iterator of responses
-    # (thanks to HTTP/2).
-    for response in responses:
-        print(
-            f"Industry id '{response.industry_id}' streamed for requested account '{account_id}'"
-        )
+    # don't forget to close the channel when done!
+    channel.close()
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
-    with grpc.insecure_channel("localhost:50051") as channel:
-        # We create a channel to the server.
-        #
-        # We then create an instance of the stub so that we can
-        # call our RPC methods.
-        stub = account_pb2_grpc.AccountStub(channel)
-
-        # 1. Should return a `GetAccountResponse` of the given `account_id`
-        print("GetAccount 123:")
-        get_account(stub, 123)
-        print()
-
-        print("GetAccount 456:")
-        get_account(stub, 456)
-        print()
-        print()
-
-        # 2. Should stream a list of industry ids one by one
-        print("StreamIndustryIds:")
-        stream_industry_ids(stub, 789)
-        print()
+    asyncio.run(main())
